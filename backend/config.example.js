fe945bac01578369432d44a80dcc49d89696abca
Satyam Jha <162445378+Sat2005@users.noreply.github.com>
2025-06-17 16:58:38 +0530

    Create config.example.js

---
+++ backend/config.example.js
@@ -0,0 +1,12 @@
+// config.example.js
+module.exports = {
+  STRIPE_SECRET_KEY: "your_stripe_secret_here",
+  MONGO_URI: "your_mongo_uri_here",
+  CLOUDINARY: {
+    cloud_name: "your_cloud_name",
+    api_key: "your_api_key",
+    api_secret: "your_api_secret"
+  },
+  JWT_USER_PASSWORD: "your_jwt_user_key",
+  JWT_ADMIN_PASSWORD: "your_jwt_admin_key",
+};
